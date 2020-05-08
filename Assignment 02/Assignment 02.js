0f748986207d8fc21dbff8c8c19260798e9e4f38
ahsanazeemi <funwithahsanazeemi@gmail.com>
2020-05-08 19:00:49 +0500

    Saylani Website Complete

---
+++ Assignment 02/Assignment 02.js
@@ -0,0 +1,27 @@
+/*****
+ *Javascript Codes  
+ *****/
+function clickfun() {
+    document.getElementById("first").innerHTML = "max 30 characters a-z and A-Z";
+}
+function clickfun1() {
+    document.getElementById("second").innerHTML = "max 30 characters a-z and A-Z";
+}
+function clickfun2() {
+    document.getElementById("third").innerHTML = "Enter a Valid Email Adress";
+}
+function clickfun3() {
+    document.getElementById("fth").innerHTML = "Enter 10 Digit Mobile Number";
+}
+function clickfun4() {
+    document.getElementById("fith").innerHTML = "max 30 characters a-z and A-Z";
+}
+function clickfun5() {
+    document.getElementById("sth").innerHTML = "Enter Your Pin Code";
+}
+function clickfun6() {
+    document.getElementById("seth").innerHTML = "max 30 characters a-z and A-Z";
+}
+function clickfun7() {
+    document.getElementById("eith").innerHTML = "Enter Your Country Name";
+}
